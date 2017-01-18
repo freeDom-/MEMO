be709a4e25b1dc7e2b48244f4b700e3dd75dd66b
freeDom- <dominik.weinrich@gmx.de>
2017-01-18 15:00:10 +0100

    inserted data for Memo class

--- app/src/main/java/com/example/ffrae_000/memo/Memo.java
+++ app/src/main/java/com/example/ffrae_000/memo/Memo.java
@@ -1,5 +1,32 @@
 package com.example.ffrae_000.memo;
 
 
+import java.util.Date;
+
 public class Memo {
+    private int id;
+    private String name;
+    private Date date;
+
+    Memo(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.date = new Date();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
