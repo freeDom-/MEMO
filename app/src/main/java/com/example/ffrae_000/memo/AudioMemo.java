ff170ba6329331c6e4b5c775ae635cb0f814d417
freeDom- <dominik.weinrich@gmx.de>
2017-01-18 15:06:24 +0100

    added constructor to AudioMemo and TextMemo to fix errors

--- app/src/main/java/com/example/ffrae_000/memo/AudioMemo.java
+++ app/src/main/java/com/example/ffrae_000/memo/AudioMemo.java
@@ -2,4 +2,8 @@ package com.example.ffrae_000.memo;
 
 
 public class AudioMemo extends Memo {
+
+    AudioMemo(int id, String name) {
+        super(id, name);
+    }
 }
--- app/src/main/java/com/example/ffrae_000/memo/TextMemo.java
+++ app/src/main/java/com/example/ffrae_000/memo/TextMemo.java
@@ -2,4 +2,8 @@ package com.example.ffrae_000.memo;
 
 
 public class TextMemo extends Memo {
+
+    TextMemo(int id, String name) {
+        super(id, name);
+    }
 }
