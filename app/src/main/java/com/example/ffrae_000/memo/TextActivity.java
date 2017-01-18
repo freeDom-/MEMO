f57d4de1647e55746277b4ddb51a2d00bbcb237c
freeDom- <dominik.weinrich@gmx.de>
2017-01-18 15:52:05 +0100

    added functionality to TextActivity

--- app/src/main/java/com/example/ffrae_000/memo/TextActivity.java
+++ app/src/main/java/com/example/ffrae_000/memo/TextActivity.java
@@ -1,18 +1,33 @@
 package com.example.ffrae_000.memo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.KeyListener;
 import android.view.View;
+import android.widget.EditText;
 
 public class TextActivity extends AppCompatActivity {
+
+    private TextMemo memo;
+    private EditText etData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text);
+
+        Intent intent = getIntent();
+        memo = (TextMemo) intent.getSerializableExtra("TextMemo");
+
+        // Load text from Memo
+        etData = (EditText) findViewById(R.id.editTextData);
+        etData.setText(memo.getData());
+        etData.setKeyListener(null);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -20,8 +35,13 @@ public class TextActivity extends AppCompatActivity {
         fabSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // Save data into the object and return code 1337 for saving the changes to a file
+                memo.setData(etData.getText().toString());
+
+                Intent intent = new Intent();
+                intent.putExtra("TextMemo", memo);
+                setResult(1337, intent);
+                finish();
             }
         });
 
@@ -29,8 +49,7 @@ public class TextActivity extends AppCompatActivity {
         fabSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                etData.setKeyListener((KeyListener) etData.getTag());
             }
         });
     }
