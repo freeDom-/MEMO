ab12ac2495bf8a28f60451aff51c0c3254d08d5e
sir_cancle_a_lot <f.fraeulin@gmx.de>
2017-01-18 18:18:51 +0100

    AudioMemo written

--- app/src/main/java/com/example/ffrae_000/memo/AudioMemo.java
+++ app/src/main/java/com/example/ffrae_000/memo/AudioMemo.java
@@ -1,9 +1,30 @@
 package com.example.ffrae_000.memo;
 
 
-public class AudioMemo extends Memo {
+import android.media.MediaRecorder;
+import android.media.MediaPlayer;
 
-    AudioMemo(int id, String name) {
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class AudioMemo extends Memo {
+    private MediaRecorder mRecorder = null;
+    private String dir = "/SD-Karte/Media/audio/MEMO-audio/";
+    private String path;
+    AudioMemo(int id, String name){
         super(id, name);
+        path = dir+name+id+ ".3GPP";
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setDir(String dir) {
+        this.dir = dir;
     }
 }
